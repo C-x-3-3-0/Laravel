c6c54fd7b623a18995b39da0644cbbb9b121affa
Hao Wang <89629020+C-x-3-3-0@users.noreply.github.com>
2023-03-08 15:46:38 +0100

    works saved into heidi sql

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,24 @@ Route::get('/form', function () {
     return view('forms');
 });
 
+Route::post('/form', function () {
+    $request = request();
+
+    $application = new \App\Models\Application();
+    $application -> email = $request -> get('email');
+    $application -> lastname = $request -> get('lastname');
+    $application -> firstname = $request -> get('firstname');
+    $application -> title = $request -> get('title');
+    $application -> date = $request -> get('date');
+
+    $application -> save();
+
+    return redirect('/form');
+});
+
+Route::get('/form/applications', function () {
+    $applications = \App\Models\Allpication::all();
+
+    dd($application);
+});
+
